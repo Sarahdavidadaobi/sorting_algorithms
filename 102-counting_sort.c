201721fb6651d7efa18c3eb28742da0b4616611f
root <davidsarah008@gmail.com>
2023-10-30 16:17:32 -0700

    sorting algorithms

---
+++ 102-counting_sort.c
@@ -0,0 +1,21 @@
+#include "sort.h"
+
+/**
+ * get_max - Get the maximum value in an array of integers.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Return: The maximum integer in the array.
+ */
+int get_max(int *array, int size)
+{
+	int max, i;
+
+	for (max = array[0], i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	return (max);
+}
